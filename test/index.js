f7de645174f573fa9ebf7df04a14ce80f12aaf05
rajikaimal <inbox.rajika@gmail.com>
2016-12-17 13:59:50 +0530

    add tests

--- test/index.js
+++ test/index.js
@@ -1,7 +1,16 @@
 import {assert} from 'chai';
+import ferreter from '../src/index';
 
 describe('test core', function () {
-		it('should ping server', function (done) {
-			
-		});
+	it('should ping server', function (done) {
+		ferreter('http://google.com', {'something': 'something'})
+			.then((res) => {
+				assert.typeOf(res, 'object', 'response object');
+				done();
+			})
+			.catch((err) => {
+				assert.typeOf(err, 'object', 'error response object');
+				done();
+			});
+	});
 });
